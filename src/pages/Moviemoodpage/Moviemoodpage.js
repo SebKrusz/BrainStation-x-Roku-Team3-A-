3ec0b6a5bb1948ef183aa7aa09be83903c9e97c0
JonathanBattista <145586415+JonathanBattista@users.noreply.github.com>
2023-11-17 14:37:09 -0500

    idk its fucked

--- src/pages/Moviemoodpage/Moviemoodpage.js
+++ src/pages/Moviemoodpage/Moviemoodpage.js
@@ -1,5 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export const Moviemoodpage = () => {
-  return <div></div>;
+  const [movieData, setMovieData] = useState([]);
+  const { selectedGenre } = useParams();
+
+  useEffect(() => {
+    if (selectedGenre) {
+      axios
+        .get(`http://localhost:8080/genre/${selectedGenre}`)
+        .then(response => {
+          setMovieData(response.data);
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error("Error fetching movie data:", error);
+        });
+    }
+  }, [selectedGenre]);
+
+  return (
+    <div>
+      <h1>Choose a genre to see movies:</h1>
+      <label>Select a genre:</label>
+      <select id="genre" name="genre" value={selectedGenre}>
+        <option value="">-- Select Genre --</option>
+        {movieData.map(genreData => (
+          <option key={genreData.genre} value={genreData.genre}>
+            {genreData.genre}
+          </option>
+        ))}
+      </select>
+
+      {selectedGenre && (
+        <div>
+          <h2>Movies in the {selectedGenre} genre:</h2>
+          <ul>
+            {movieData.map(movie => (
+              <li key={movie.title}>
+                <h3>{movie.title}</h3>
+                <p>Year: {movie.year}</p>
+                <img src={movie.image} alt={movie.title} />
+                <p>{movie.description}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 };
