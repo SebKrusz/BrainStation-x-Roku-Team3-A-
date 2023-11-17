c93abc87fc3fef7e7d89c187d09d7ed5a71ca9f0
SebKrusz <145293986+SebKrusz@users.noreply.github.com>
2023-11-17 11:57:39 -0800

    potential revert

--- src/pages/Moviemoodpage/Moviemoodpage.js
+++ src/pages/Moviemoodpage/Moviemoodpage.js
@@ -1,18 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export const Moviemoodpage = () => {
   const [movieData, setMovieData] = useState([]);
   const { selectedGenre } = useParams();
+  const navigate = useNavigate();
+
+  const handleGenreChange = event => {
+    const selectedValue = event.target.value;
+    navigate(`/genre/${selectedValue}`);
+  };
 
   useEffect(() => {
     if (selectedGenre) {
       axios
         .get(`http://localhost:8080/genre/${selectedGenre}`)
         .then(response => {
+          console.log("API Response:", response.data);
           setMovieData(response.data);
-          console.log(response.data);
         })
         .catch(error => {
           console.error("Error fetching movie data:", error);
@@ -24,13 +30,15 @@ export const Moviemoodpage = () => {
     <div>
       <h1>Choose a genre to see movies:</h1>
       <label>Select a genre:</label>
-      <select id="genre" name="genre" value={selectedGenre}>
-        <option value="">-- Select Genre --</option>
-        {movieData.map(genreData => (
-          <option key={genreData.genre} value={genreData.genre}>
-            {genreData.genre}
-          </option>
-        ))}
+      <select id="genre" name="genre" value={selectedGenre} onChange={handleGenreChange}>
+        <option value="" disabled>
+          -- Select Genre --
+        </option>
+        <option value="Action">Action</option>
+        <option value="Comedy">Comedy</option>
+        <option value="Thriller">Thriller</option>
+        <option value="Romance">Romance</option>
+        <option value="Documentaries">Documentaries</option>
       </select>
 
       {selectedGenre && (
