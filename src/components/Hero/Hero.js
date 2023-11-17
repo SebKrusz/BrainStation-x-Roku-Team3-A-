b49e62c7f4cfe68267dc820faaf08ff05ea6f847
Nate Paradiso <ipfcolo05@gmail.com>
2023-11-17 09:35:43 -0800

    hero done

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,20 @@
 import React from "react";
 import "./Hero.scss";
+import image from "../../assets/images/image.png";
+import "../../styles/global.scss";
 
 export const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div>
+      <div className="hero">
+        <div className="hero__text">
+          <h1 className="hero__text--title">Find something to watch before your food gets cold.</h1>
+          <h4>Streaming recommendations catered to you in a convenient way.</h4>
+        </div>
+        <div className="hero__image">
+          <img className="hero__image--image" src={image} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
