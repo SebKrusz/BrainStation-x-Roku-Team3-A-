d5d98ab9953e05e2d2db25b455c8da925d6be238
SebKrusz <145293986+SebKrusz@users.noreply.github.com>
2023-11-17 10:53:53 -0800

    More Styles and work done

--- src/components/Moviemoodhome/Moviemoodhome.js
+++ src/components/Moviemoodhome/Moviemoodhome.js
@@ -1,20 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Moviemoodhome.scss";
+import movie1 from "../../assets/images/action/dieHard.jpg";
+import movie2 from "../../assets/images/action/inception.jpg";
+import movie3 from "../../assets/images/action/madmax.jpg";
+import movie4 from "../../assets/images/action/thedarkknight.jpg";
+import movie5 from "../../assets/images/action-thriller/bourneIdentity.jpg";
+import movie6 from "../../assets/images/action-thriller/johnwick.jpg";
+import movie7 from "../../assets/images/action-thriller/killbill.jpg";
 
 export const Moviemoodhome = () => {
   return (
-    <div>
-      <h3>Say goodbye to endless scrolling and hello to personalized movie magic</h3>
-      <p>
+    <div className="moviemood-home-container">
+      <h3 className="title">
+        Say goodbye to endless scrolling and hello to personalized movie magic
+      </h3>
+      <p className="description">
         Our unique assessment is your passport to a tailored movie experience. By understanding your
         current vibe, we handpick a selection of films perfectly aligned with your mood.
       </p>
-      <Link to="/Moviemood">
-        <button>FIND A MOVIE TO WATCH</button>
+      <Link to="/Moviemood" className="button-link">
+        <button className="find-movie-button">FIND A MOVIE TO WATCH</button>
       </Link>
-      <p>
-        People spend an average on 7.4 minutes deciding what to watch. We’ll make it easier for you.
+      <div className="image-gallary">
+        <img src={movie1} className="movieposter"></img>
+        <img src={movie2} className="movieposter"></img>
+        <img src={movie3} className="movieposter"></img>
+        <img src={movie4} className="movieposter-spec"></img>
+        <img src={movie5} className="movieposter"></img>
+        <img src={movie6} className="movieposter"></img>
+        <img src={movie7} className="movieposter"></img>
+      </div>
+      <p className="note">
+        People spend an average of 7.4 minutes deciding what to watch. We’ll make it easier for you.
       </p>
     </div>
   );
